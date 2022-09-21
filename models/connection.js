a1980f86256c44fe9d1cb0188045af44516ad57c
Marine Mellado <marine.mellado@gmail.com>
2022-09-21 09:18:30 +0200

    first commit - initialized express backend

---
+++ models/connection.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var options = {
+    connectTimeoutMS: 5000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+const connectionString = process.env.CONNECTION_STRING;
+
+mongoose.connect(connectionString,
+    options,
+    function (err) {
+        console.log(err);
+    }
+);
+
